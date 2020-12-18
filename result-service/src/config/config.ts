ac2f53e3a9d237f353da775c9aa190b2b6f06f98
Jakub Zygmunt <216938@edu.p.lodz.pl>
2020-12-18 22:06:17 +0100

    Add answer service

---
+++ result-service/src/config/config.ts
@@ -0,0 +1,16 @@
+import { POSTGRES_CONFIG } from '../app.constants';
+
+export default () => ({
+  port: parseInt(process.env.RESULT_SERVICE_PORT, 10) || 3000,
+  [POSTGRES_CONFIG]: {
+    type: 'postgres',
+    host: process.env.POSTGRES_RESULT_SERVICE_DB_HOSTNAME,
+    port: process.env.POSTGRES_RESULT_SERVICE_DB_PORT,
+    username: process.env.POSTGRES_RESULT_SERVICE_USERNAME,
+    password: process.env.POSTGRES_RESULT_SERVICE_PASSWORD,
+    database: process.env.POSTGRES_RESULT_SERVICE_DB,
+    synchronize: true,
+    logging: true,
+    entities: ['dist/**/*.entity{.ts,.js}'],
+  },
+});
---
+++ result-service/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 8084,
+        host: 'result-service',
+      },
+    },
+  );
+  const configService = app.get(ConfigService);
+  const port = configService.get('port');
+  console.log(`Starting RESULT service on ${port}`);
+  await app.listen(port);
+}
+bootstrap();
